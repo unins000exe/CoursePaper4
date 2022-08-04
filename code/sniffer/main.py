2c6eeea9e64e40c61e4b07d9265894c730b406a6
Romario <roman20011102@gmail.com>
2022-08-04 20:56:32 +0400

    Добавил определение сетевых интерфейсов и их вывод

--- code/sniffer/main.py
+++ code/sniffer/main.py
@@ -2,11 +2,16 @@
 import tkinter as tk
 from tkinter import ttk
 import socket
+
+import netifaces
+
 import sniffer
 from datetime import datetime
 import os
 import sys
-import sqlite3
+import netifaces as ni
+import winreg as wr
+from pprint import pprint
 
 TAB_2 = '\t * '
 
@@ -19,65 +24,119 @@ class Menu(tk.Frame):
         self.last_time = ''
         self.output_list = []
 
-        self.frame_out = ttk.LabelFrame(self, text='Вывод TCP/UDP трафика')
-        self.frame_out.grid(row=0, column=0)
+        self.frame_choose_interface = ttk.Frame(self, width=150, height=75)
+        self.frame_choose_interface.grid(row=0, column=0)
 
-        self.columns = ['1', '2', '3', '4', '5', '6']
-        self.output = ttk.Treeview(self, show='headings', columns=self.columns, height=25)
-        self.output.heading('1', text='Время')
-        self.output.heading('2', text='Источник')
-        self.output.heading('3', text='Назначение')
-        self.output.heading('4', text='Протокол')
-        self.output.heading('5', text='Длина')
-        self.output.heading('6', text='Инфо') # TODO: не вмещается
+        self.label_choose_interface = ttk.Label(self.frame_choose_interface,
+                                                text='Выберите интерфейс для прослушивания')
+        self.label_choose_interface.grid(row=0, column=0)
 
-        self.output.column('1', minwidth=0, width=55)
-        self.output.column('2', minwidth=0, width=125)
-        self.output.column('3', minwidth=0, width=125)
-        self.output.column('4', minwidth=0, width=75)
-        self.output.column('5', minwidth=0, width=50)
-        self.output.column('6', minwidth=0, width=150)
+        # self.list_of_interfaces = tk.Listbox(self.frame_choose_interface, width=50, height=10)
+        # self.list_of_interfaces.grid(row=1, column=0)
+        #
+        # for inter in inters_ips:
+        #     self.list_of_interfaces.insert('end', (inter, inters_ips[inter]))
 
-        self.output.grid(row=0, column=0, padx=(5, 0), sticky=tk.NW)
+        self.loi_columns = ['1', '2']
+        self.list_of_interfaces = ttk.Treeview(self.frame_choose_interface,
+                                               show='headings', columns=self.loi_columns, height=10)
+        self.list_of_interfaces.heading('1', text='Интерфейс')
+        self.list_of_interfaces.heading('2', text='IP-адрес')
+        self.list_of_interfaces.grid(row=1, column=0)
 
-        self.scroll_out = ttk.Scrollbar(self.frame_out, command=self.output.yview)
-        self.output.config(yscrollcommand=self.scroll_out.set)
+        for inter in inters_ips:
+            self.list_of_interfaces.insert(parent='', index='end', values=[inter, inters_ips[inter]])
 
-        self.frame = ttk.LabelFrame(self, text='Список адресов, взаимодействующих через P2P')
-        self.frame.grid(row=0, column=1)
+        # self.choose_btn = ttk.Button(self.frame_choose_interface, text='Выбрать', command=self.choose_interface)
+        # self.choose_btn.grid(row=2, column=0)
 
-        self.label2 = ttk.Label(self.frame, text='Анализ портов')
-        self.label2.grid(row=0, column=0, pady=5, sticky=tk.N)
+        self.list_of_interfaces.bind('<<TreeviewSelect>>', self.create_socket)
 
-        self.p2p_lb = tk.Listbox(self.frame, height=20)
-        self.p2p_lb.grid(row=1, column=0, sticky=tk.N)
 
-        self.scroll_p2p_lb = ttk.Scrollbar(self.frame, command=self.output.yview)
-        self.p2p_lb.config(yscrollcommand=self.scroll_p2p_lb.set)
+        # self.frame_main = ttk.Frame(self)
+        # self.frame_main.grid(row=0, column=0)
+        #
+        # self.columns = ['1', '2', '3', '4', '5', '6', '7']
+        # self.output = ttk.Treeview(self.frame_main, show='headings', columns=self.columns, height=25)
+        # self.output.heading('1', text='Время')
+        # self.output.heading('2', text='Источник')
+        # self.output.heading('3', text='Назначение')
+        # self.output.heading('4', text='Порты')
+        # self.output.heading('5', text='Протокол')
+        # self.output.heading('6', text='Длина')
+        # self.output.heading('7', text='Инфо')
+        #
+        # self.output.column('1', minwidth=0, width=55)
+        # self.output.column('2', minwidth=0, width=100)
+        # self.output.column('3', minwidth=0, width=100)
+        # self.output.column('4', minwidth=0, width=125)
+        # self.output.column('5', minwidth=0, width=70)
+        # self.output.column('6', minwidth=0, width=50)
+        # self.output.column('7', minwidth=0, width=150)
+        #
+        # self.output.grid(row=0, column=0, padx=(5, 0), sticky=tk.NW)
+        #
+        # self.scroll_out = ttk.Scrollbar(self.self.frame_main, command=self.output.yview)
+        # self.output.config(yscrollcommand=self.scroll_out.set)
+        #
+        # self.frame = ttk.LabelFrame(self.frame_main, text='Список адресов, взаимодействующих через P2P')
+        # self.frame.grid(row=0, column=1)
+        #
+        # self.label2 = ttk.Label(self.frame, text='Анализ портов')
+        # self.label2.grid(row=0, column=0, pady=5, sticky=tk.N)
+        #
+        # self.p2p_lb = tk.Listbox(self.frame, height=20)
+        # self.p2p_lb.grid(row=1, column=0, sticky=tk.N)
+        #
+        # self.scroll_p2p_lb = ttk.Scrollbar(self.frame, command=self.output.yview)
+        # self.p2p_lb.config(yscrollcommand=self.scroll_p2p_lb.set)
+        #
+        # self.label3 = ttk.Label(self.frame, text='IP/Port-эвристика')
+        # self.label3.grid(row=0, column=1, pady=5, sticky=tk.N)
+        #
+        # self.p2p_lb2 = tk.Listbox(self.frame, height=20)
+        # self.p2p_lb2.grid(row=1, column=1, sticky=tk.N, padx=5)
+        #
+        # self.scroll_p2p_lb2 = ttk.Scrollbar(self.frame, command=self.output.yview)
+        # self.p2p_lb2.config(yscrollcommand=self.scroll_p2p_lb.set)
+        #
+        # self.label4 = ttk.Label(self.frame, text='TCP/UDP-эвристика')
+        # self.label4.grid(row=0, column=2, pady=5, sticky=tk.N)
+        #
+        # self.p2p_lb3 = tk.Listbox(self.frame, height=20)
+        # self.p2p_lb3.grid(row=1, column=2, sticky=tk.N)
+        #
+        # self.scroll_p2p_lb3 = ttk.Scrollbar(self.frame, command=self.output.yview)
+        # self.p2p_lb3.config(yscrollcommand=self.scroll_p2p_lb.set)
+        #
+        # self.stop_btn = ttk.Button(self.frame_main, text='Стоп', command=self.stop)
+        # self.stop_btn.grid(row=1, column=0, pady=(10, 10))
+        #
+        # self.call_sniff()
+        # self.call_find_p2p()
 
-        self.label3 = ttk.Label(self.frame, text='IP/Port-эвристика')
-        self.label3.grid(row=0, column=1, pady=5, sticky=tk.N)
-
-        self.p2p_lb2 = tk.Listbox(self.frame, height=20)
-        self.p2p_lb2.grid(row=1, column=1, sticky=tk.N, padx=5)
-
-        self.scroll_p2p_lb2 = ttk.Scrollbar(self.frame, command=self.output.yview)
-        self.p2p_lb2.config(yscrollcommand=self.scroll_p2p_lb.set)
-
-        self.label4 = ttk.Label(self.frame, text='TCP/UDP-эвристика')
-        self.label4.grid(row=0, column=2, pady=5, sticky=tk.N)
-
-        self.p2p_lb3 = tk.Listbox(self.frame, height=20)
-        self.p2p_lb3.grid(row=1, column=2, sticky=tk.N)
-
-        self.scroll_p2p_lb3 = ttk.Scrollbar(self.frame, command=self.output.yview)
-        self.p2p_lb3.config(yscrollcommand=self.scroll_p2p_lb.set)
-
-        self.stop_btn = ttk.Button(self, text='Стоп', command=self.stop)
-        self.stop_btn.grid(row=1, column=0, pady=(10, 10))
-
-        self.call_sniff()
-        self.call_find_p2p()
+    def create_socket(self):
+        select = self.list_of_interfaces.selection()[0]
+        item = self.list_of_interfaces.item(select)
+        try:
+            if os.name == 'nt':
+                os = False
+                interface = item['values'][1]
+                conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+                conn.bind((interface, 0))
+                conn.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+                conn.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+            else:
+                os = True
+                interface = 'enp0s3'
+                if len(sys.argv) > 1:
+                    interface = sys.argv[1]
+                os.system("ip link set {} promisc on".format(interface))  # ret =
+                conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+                conn.bind((interface, 0))
+        except socket.error as msg:
+            print('Сокет не может быть создан. Код ошибки : ' + str(msg[0]) + ' Сообщение ' + msg[1])
+            sys.exit()
 
     def call_sniff(self):
         out = sniffer.sniff(conn, os)
@@ -88,7 +147,7 @@ class Menu(tk.Frame):
                 # self.output.insert('end', time)
                 file.write(time)
             self.last_time = time
-            ins = [time, out[2] + ':' + out[4], out[6] + ':' + out[8], out[1], out[10] + ' Б', '']
+            ins = [time, out[2], out[6], out[4] + ' -> ' + out[8], out[1], out[10] + ' Б', '']
             self.output_list.append(ins)
             self.output.insert(parent='', index='end', values=ins)
 
@@ -113,21 +172,21 @@ class Menu(tk.Frame):
         for addr in sniffer.p2p_addrs:
             self.p2p_lb3.insert('end', addr[0] + ":" + str(addr[1]))
 
-        for row in self.output.get_children():
-            cur_val = list(self.output.item(row, 'values'))
-            ip1, port1 = cur_val[1].split(':')
-            ip2, port2 = cur_val[2].split(':')
-            port1 = int(port1)
-            port2 = int(port2)
-            if (ip1, port1) in sniffer.p2p_pairs_p or (ip2, port2) in sniffer.p2p_pairs_p:
-                cur_val[5] += 'P2P-порты\n'
-                self.output.item(row, values=cur_val)
-            if (ip1, port1) in sniffer.p2p_pairs_ipp or (ip2, port2) in sniffer.p2p_pairs_ipp:
-                cur_val[5] += 'IP/Port-эвристика\n'
-                self.output.item(row, values=cur_val)
-            if (ip1, port1) in sniffer.p2p_addrs or (ip2, port2) in sniffer.p2p_addrs:
-                cur_val[5] += 'TCP/UDP-эвристика\n'
-                self.output.item(row, values=cur_val)
+        # for row in self.output.get_children():
+        #     cur_val = list(self.output.item(row, 'values'))
+        #     ip1, port1 = cur_val[1].split(':')
+        #     ip2, port2 = cur_val[2].split(':')
+        #     port1 = int(port1)
+        #     port2 = int(port2)
+        #     if (ip1, port1) in sniffer.p2p_pairs_p or (ip2, port2) in sniffer.p2p_pairs_p:
+        #         cur_val[5] += 'P2P-порты\n'
+        #         self.output.item(row, values=cur_val)
+        #     if (ip1, port1) in sniffer.p2p_pairs_ipp or (ip2, port2) in sniffer.p2p_pairs_ipp:
+        #         cur_val[5] += 'IP/Port-эвристика\n'
+        #         self.output.item(row, values=cur_val)
+        #     if (ip1, port1) in sniffer.p2p_addrs or (ip2, port2) in sniffer.p2p_addrs:
+        #         cur_val[5] += 'TCP/UDP-эвристика\n'
+        #         self.output.item(row, values=cur_val)
 
         root.after(15000, self.call_find_p2p)
 
@@ -148,32 +207,37 @@ class Menu(tk.Frame):
 
 
 if __name__ == "__main__":
-    try:
-        if os.name == 'nt':
-            os = False
-            interface = '192.168.1.100'
-            conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
-            conn.bind((interface, 0))
-            conn.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-            conn.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-        else:
-            os = True
-            interface = 'enp0s3'
-            if len(sys.argv) > 1:
-                interface = sys.argv[1]
-            os.system("ip link set {} promisc on".format(interface))  # ret =
-            conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
-            conn.bind((interface, 0))
-    except socket.error as msg:
-        print('Сокет не может быть создан. Код ошибки : ' + str(msg[0]) + ' Сообщение ' + msg[1])
-        sys.exit()
+    def get_connection_name_from_guid(iface_guids):
+        iface_names = ['(unknown)' for i in range(len(iface_guids))]
+        reg = wr.ConnectRegistry(None, wr.HKEY_LOCAL_MACHINE)
+        reg_key = wr.OpenKey(reg, r'SYSTEM\CurrentControlSet\Control\Network\{4d36e972-e325-11ce-bfc1-08002be10318}')
+        for i in range(len(iface_guids)):
+            try:
+                reg_subkey = wr.OpenKey(reg_key, iface_guids[i] + r'\Connection')
+                iface_names[i] = wr.QueryValueEx(reg_subkey, 'Name')[0]
+            except FileNotFoundError:
+                pass
+        return iface_names
+
+
+    x = ni.interfaces()
+    interfaces = []
+    ips = []
+    for interface in x:
+        addr = ni.ifaddresses(interface)
+        try:
+            ip = addr[ni.AF_INET][0]['addr']
+            interfaces.append(interface)
+            ips.append(ip)
+        except:
+            pass
+    interfaces = get_connection_name_from_guid(interfaces)
+    inters_ips = dict(zip(interfaces, ips))
 
     # В файл сохраняется последний вывод программы
     file = open('out.txt', 'w+')
     # Список IP-адресов, взаимодействующих через P2P
     file2 = open('ip_list.txt', 'w+')
-
-    # db = sqlite3.connect('sniffer.db')
 
     root = tk.Tk()
     root.title("Анализатор сетевого трафика")
